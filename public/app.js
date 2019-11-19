ee468aade5ec512b08a635647a539d26b57aa927
Chris Davies <c.m.davies10@gmail.com>
2019-11-19 16:47:18 -0500

    updated to test branch

--- public/app.js
+++ public/app.js
@@ -1,23 +1,23 @@
-$.getJSON("/articles", function (data) {
-	// For each one
-	for (var i = 0; i < data.length; i++) {
-		$("#articles").append(
-			// "<div data-id='" + data[i]._id + "'>" +
-			"<br/><img src='" + data[i].img + "' height='400' width='700'><br/><br/>" +
-			"<h3>" + data[i].title + "</h3><br/>" +
-			"<p>" + data[i].summary + " | " +
-			"<a href='" + "https://www.nytimes.com" + data[i].link + "'>" + "Full Article</a></p><br/>" +
-			// "<p id='comments'></p><br/>" +
-			// "<textarea placeholder='Insert comment'></textarea>" +
-			"<button data-id='" + data[i]._id + "' id='add-comment'>Comment</button><br/><hr/>");
-		// "<p>" + "<a href='/articles/" + data[i]._id + "' id='add-comment'>Comment</a>" + " | " +
-		// "<a href='" + data[i]._id + "'>" + "Edit Comment</a>" + " | " +
+// $.getJSON("/articles", function (data) {
+// 	// For each one
+// 	for (var i = 0; i < data.length; i++) {
+// 		$("#articles").append(
+// 			// "<div data-id='" + data[i]._id + "'>" +
+// 			"<br/><img src='" + data[i].img + "' height='400' width='700'><br/><br/>" +
+// 			"<h3>" + data[i].title + "</h3><br/>" +
+// 			"<p>" + data[i].summary + " | " +
+// 			"<a href='" + "https://www.nytimes.com" + data[i].link + "'>" + "Full Article</a></p><br/>" +
+// 			// "<p id='comments'></p><br/>" +
+// 			// "<textarea placeholder='Insert comment'></textarea>" +
+// 			"<button data-id='" + data[i]._id + "' id='add-comment'>Comment</button><br/><hr/>");
+// 		// "<p>" + "<a href='/articles/" + data[i]._id + "' id='add-comment'>Comment</a>" + " | " +
+// 		// "<a href='" + data[i]._id + "'>" + "Edit Comment</a>" + " | " +
 
-		// "<form action='/articles/" + data[i]._id + "' method='POST'>" +
-		// "<textarea></textarea>" + "<br/>" +
-		// "<button data-id='" + data[i]._id + "'>Add Comment</button>" + "</form > " + "<br/><hr/>");
-	}
-});
+// 		// "<form action='/articles/" + data[i]._id + "' method='POST'>" +
+// 		// "<textarea></textarea>" + "<br/>" +
+// 		// "<button data-id='" + data[i]._id + "'>Add Comment</button>" + "</form > " + "<br/><hr/>");
+// 	}
+// });
 
 $(document).on("click", "#add-comment", function () {
 	$("#comments").empty();
@@ -33,13 +33,14 @@ $(document).on("click", "#add-comment", function () {
 		console.log(data);
 
 		$("#comments").append(
-			"<h5>" + data.title + "</h5><br/>" +
-			// "<input id='titleinput' name='title'><br/>" +
-			"<textarea id='bodyinput' name='body'></textarea><br/>" +
-			"<span><button data-id='" + data._id + "' id='savecomment'>Save Comment</button>" +
+			// 	// "<h5>" + data.title + "</h5><br/>" +
+			// 	// "<input id='titleinput' name='title'><br/>" +
+			"<textarea id='bodyinput' name='body' cols='150' rows='2'></textarea><br/>" +
+			"<button class='btn btn-danger' id='savecomment' data-id='" + data._id + "'>Save Comment</button>"
+			// 	"<span><button data-id='" + data._id + "' id='savecomment'>Save Comment</button>"
 
-			// UPDATE data._id vs. data.comment._id ================
-			"<button data-id='" + data.comment._id + "' id='deletecomment'>Delete Comment</button></span>"
+			// 	// UPDATE data._id vs. data.comment._id ================
+			// 	// "<button data-id='" + data.comment._id + "' id='deletecomment'>Delete Comment</button></span>"
 		);
 		// } else {
 		// 	$("#comments").append(
@@ -82,7 +83,20 @@ $(document).on("click", "#savecomment", function () {
 		// Log the response
 		console.log(data);
 
-		$("#comments").empty();
+		// $("#comments").append(
+		// 	"<br/><P><strong>Comment</strong></P>" +
+		// 	"<p id='bodyinput></p>"
+
+		// )
+
+		// $("#comments").empty();
+
+		if (data.comment) {
+			// Place the title of the note in the title input
+			$("#titleinput").val(data.comment.title); // not using
+			// Place the body of the note in the body textarea
+			$("#bodyinput").val(data.comment.body);
+		};
 	});
 
 	// Also, remove the values entered in the input and textarea for note entry
