4b1b55d7f496fd23aca45638f1e93071be4d43cf
Chris Davies <c.m.davies10@gmail.com>
2019-11-18 21:02:04 -0500

    update to styling and data placement - ready for comments

--- public/app.js
+++ public/app.js
@@ -9,11 +9,6 @@
 $.getJSON("/articles", function (data) {
 	// For each one
 	for (var i = 0; i < data.length; i++) {
-
-		// $("#articles").addClass("card")
-		// $("#articles-card").addClass("card-header")
-		// $(".card-header").text(data[i].title);
-		// Display the apropos information on the page
-		$("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "</p>" + "<br />" + "<p>" + data[i].summary + "</p>" + "<br/>" + "<p>" + "<a href='#'>" + "Comment" + "</a>" + " | " + "<a href='" + "https://www.nytimes.com" + data[i].link + "'>" + "Read Full Article" + "</a>" + "</p>" + "<br/>" + "<hr/>");
+		$("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "</p>" + "<br />" + "<p>" + data[i].summary + "</p>" + "<br/>" + "<p>" + "<a href='#' id='add-comment'>" + "Comment" + "</a>" + " | " + "<a href='" + "https://www.nytimes.com" + data[i].link + "'>" + "Read Full Article" + "</a>" + "</p>" + "<br/>" + "<hr/>");
 	}
 });
